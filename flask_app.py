2f33427e8562982d34e0f7fd1dcfbfdeaeee07b3
Izzy <izzy@iso-bel.computer>
2025-12-03 23:03:04 +0000

    initial commit

---
+++ flask_app.py
@@ -0,0 +1,10 @@
+
+from datetime import datetime
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html',
+                         current_date=datetime.now().strftime('%Y-%m-%d'))
