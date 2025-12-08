3b1c8aff45906083ad2e314e7cd5c91997237221
Izzy <izzy@iso-bel.computer>
2025-12-08 21:33:18 +0000

    beginning to add blog system

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,8 @@
 
 from datetime import datetime
 from flask import Flask, render_template
+import os
+
 
 app = Flask(__name__)
 
@@ -9,6 +11,28 @@ def home():
     return render_template('base.html',
                          current_date=datetime.now().strftime('%Y-%m-%d'))
 
+
+
+def getBlockPosts():
+    posts = []
+    postsDir = 'blog/posts'
+
+    for filename in os.listDir():
+        if filename.endswith('.html'):
+            postId = filename.replace('.html','')
+            postDate = filename.split('_')[0].replace('-',' ')
+            postTitle = filename.split('_')[1].replace('-',' ')
+            posts.append({
+                'id': postId,
+                'title': postTitle,
+                'date': postDate,
+                'template': filename
+
+            })
+    return sorted(posts, key=lambda x: x['date'], reverse=True)
+
+
 @app.route('/blog')
 def blog():
-    return render_template('blog.html')
+    return render_template('blog/blog.html',
+                           posts=BLOGPOSTS)
