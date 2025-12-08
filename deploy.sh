09b317a1d33dcac1ba838e74a971699676409d15
Izzy <izzy@iso-bel.computer>
2025-12-08 20:25:22 +0000

    site updated

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git add .
+git commit -m "${1:-Quick update}"
+git push 
+ssh 1sobel@ssh.pythonanywhere.com "cd /site && git pull && touch /var/www/1sobel_pythonanywhere_com_wsgi.py"
--- flask_app.py
+++ flask_app.py
@@ -8,3 +8,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html',
                          current_date=datetime.now().strftime('%Y-%m-%d'))
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
