694529a67ffc9175e55fa6ab819ed48ef57382e0
Izzy <izzy@iso-bel.computer>
2026-02-21 13:50:43 +0000

    safari are bastards

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -1,11 +1,22 @@
 
-// if stackoverflow have fucked me over here i will kill them
-function isTouchDevice() {
-        return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+window.addEventListener("load", () => {
+    const isTouch = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+    const isNarrow = window.innerWidth < 768;
+
+    if (isTouch && isNarrow) {
+        const warning = document.createElement("div");
+        warning.innerText = "This site isn't designed for mobile.";
+        warning.style.position = "fixed";
+        warning.style.top = "0";
+        warning.style.left = "0";
+        warning.style.width = "100%";
+        warning.style.background = "red";
+        warning.style.color = "white";
+        warning.style.padding = "10px";
+        warning.style.textAlign = "center";
+        warning.style.zIndex = "9999";
+
+        document.body.appendChild(warning);
+       alert("Hello! This website isn't designed for mobile. Parts of it will work, but a lot of it will look / be kinda broken. Sorry! I would try to fix it but I am just one woman, and very tired :(")
     }
-
-console.log(isTouchDevice())
-
-if (isTouchDevice()) {
-    alert("Hello! This website isn't designed for mobile. Parts of it will work, but a lot of it will look / be kinda broken. Sorry! I would try to fix it but I am just one woman, and very tired :(")
-}
+});
