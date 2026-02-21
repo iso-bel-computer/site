71e3437a64fc239237388f02d1d6546dab8e56c8
Izzy <izzy@iso-bel.computer>
2026-02-21 13:47:49 +0000

    ok fixed

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -6,6 +6,6 @@ function isTouchDevice() {
 
 console.log(isTouchDevice())
 
-if (!isTouchDevice()) {
+if (isTouchDevice()) {
     alert("Hello! This website isn't designed for mobile. Parts of it will work, but a lot of it will look / be kinda broken. Sorry! I would try to fix it but I am just one woman, and very tired :(")
 }
