f584f83b4cb707616a86f9f243b28b8d078519e2
Izzy <izzy@iso-bel.computer>
2026-02-21 13:40:19 +0000

    i am dumb

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -4,6 +4,6 @@ function isTouchDevice() {
         return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
     }
 
-if (window.mobileCheck()) {
+if (isTouchDevice()) {
     alert("Hello! This website isn't designed for mobile. Parts of it will work, but a lot of it will look / be kinda broken. Sorry! I would try to fix it but I am just one woman, and very tired :(")
 }
