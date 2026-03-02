2dcad4ec35341d158e334aeb77ab4ed4a3c662b4
Izzy <izzy@iso-bel.computer>
2026-03-02 13:52:22 +0000

    work!

--- static/webring/onionring-widget.js
+++ static/webring/onionring-widget.js
@@ -59,11 +59,11 @@ else {
   <table>
     <tr>
       <td class='webring-prev'><a href='${sites[previousIndex]}'>← previous</a></td>
-      <td class='webring-info'>I'm part of the <a href='https://london.permacomputing.net/posts/index.html'>${ringName}</a> Club! <img src='https://www.iso-bel.computer/static/webring/flower.png'><img src='https://www.iso-bel.computer/static/webring/flower.png'><img src='https://www.iso-bel.computer/static/webring/flower.png'></br>We meet on Mondays at 6:30, @ SET Social, Peckham <br>
+      <td class='webring-info'><img src='https://www.iso-bel.computer/static/webring/flower.png'><img src='https://www.iso-bel.computer/static/webring/flower.png'><img src='https://www.iso-bel.computer/static/webring/flower.png'><br>I'm part of the <a href='https://london.permacomputing.net/posts/index.html'>${ringName}</a> Club! </br>We meet on Mondays at 6:30, @ SET Social, Peckham <br>
       <span class='webring-links'>
         ${randomText}
         ${indexText}
-        <a href='https://garlic.garden/onionring/'>what is this?</a> | <a href='https://permacomputing.net'>Wiki</span></td>
+        <a href='https://garlic.garden/onionring/'>what is this?</a> | <a href='https://permacomputing.net'>wiki</span></td>
       <td class='webring-next'><a href='${sites[nextIndex]}'>next →</a></td>
     </tr>
   </table>
