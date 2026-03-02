1d8914f0093a034800b659f33e12310fd3e7e293
Izzy <izzy@iso-bel.computer>
2026-03-02 13:34:36 +0000

    work!

--- static/webring/onionring-variables.js
+++ static/webring/onionring-variables.js
@@ -12,7 +12,7 @@ var sites = [
 ];
 
 //the name of the ring
-var ringName = 'London Permacomputing Webring';
+var ringName = 'London Permacomputing';
 
 /* the unique ID of the widget. two things to note:
  1) make sure there are no spaces in it - use dashes or underscores if you must
