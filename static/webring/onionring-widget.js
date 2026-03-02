ef72ee6d09609dfad15dcae7ab358491534a307b
Izzy <izzy@iso-bel.computer>
2026-03-02 14:57:28 +0000

    work!

--- static/webring/onionring-widget.js
+++ static/webring/onionring-widget.js
@@ -81,7 +81,7 @@ else {
             <td class='webring-prev'><a href='${sites[previousIndex]}'>←</a></td>
             <td class='webring-info'><img src='https://www.iso-bel.computer/static/webring/flower.png'><img src='https://www.iso-bel.computer/static/webring/flower.png'><img src='https://www.iso-bel.computer/static/webring/flower.png'><br>I'm in the <a href='https://london.permacomputing.net/posts/index.html'>${ringName}</a> Club! </br>We meet on Mondays at 6:30, @ <a href='https://www.openstreetmap.org/way/72032052'>SET Social, Peckham</a><br>
             <span class='webring-links'>
-                <a href='https://garlic.garden/onionring/'>what is this?</a> | <a href='https://permacomputing.net'>wiki
+                <a href='https://garlic.garden/onionring/'>what is this?</a> | <a href='https://permacomputing.net'>wiki |
                 ${randomText}
                 ${indexText}</span></td>
             <td class='webring-next'><a href='${sites[nextIndex]}'>→</a></td>
